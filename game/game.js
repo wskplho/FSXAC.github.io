9137235de8d14ef512476601701bad4ddc6e89d8
FSXAC <mansurhe@outlook.com>
2014-11-14 15:44:57 -0700

    Added win checking
    + win checking
    + swapping tiles
    + checking if the tile is placed or not

--- game/game.js
+++ game/game.js
@@ -46,79 +46,161 @@ function login() {
 }
 
 // add a block to the board
-function add(placement) {
-	if (currentPlayer == "black") {
-		switch(placement) {
+function add(placement, row, col) {
+	if (grid[row][col].src.indexOf("blank") != -1) {
+		if (currentPlayer == "black") {
+			switch(placement) {
+				
+				case "top_left":
+					grid[row][col].src = "game\\black_top_left.png";
+					break;
+				case "top_right":
+					grid[row][col].src = "game\\black_top_right.png";
+					break;
+				case "top":
+					grid[row][col].src = "game\\black_top.png";
+					break;
+				case "left":
+					grid[row][col].src = "game\\black_left.png";
+					break;
+				case "right":
+					grid[row][col].src = "game\\black_right.png";
+					break;
+				case "bottom":
+					grid[row][col].src = "game\\black_bottom.png";
+					break;
+				case "bottom_left":
+					grid[row][col].src = "game\\black_bottom_left.png";
+					break;
+				case "bottom_right":
+					grid[row][col].src = "game\\black_bottom_right.png";
+					break;
+				case "center":
+					grid[row][col].src = "game\\black.png";
+					break;
+			}	
+		} else if (currentPlayer == "white") {
+			switch(placement) {
 			
-			case "top_left":
-				grid[row][col].src = "game\\black_top_left.png";
-				break;
-			case "top_right":
-				grid[row][col].src = "game\\black_top_right.png";
-				break;
-			case "top":
-				grid[row][col].src = "game\\black_top.png";
-				break;
-			case "left":
-				grid[row][col].src = "game\\black_left.png";
-				break;
-			case "right":
-				grid[row][col].src = "game\\black_right.png";
-				break;
-			case "bottom":
-				grid[row][col].src = "game\\black_bottom.png";
-				break;
-			case "bottom_left":
-				grid[row][col].src = "game\\black_bottom_left.png";
-				break;
-			case "bottom_right":
-				grid[row][col].src = "game\\black_bottom_right.png";
-				break;
-			case "center":
-				grid[row][col].src = "game\\black.png";
-				break;
-		}	
-	} else if (currentPlayer == "white") {
-		switch(placement) {
-			
-			case "top_left":
-				grid[row][col].src = "game\\white_top_left.png";
-				break;
-			case "top_right":
-				grid[row][col].src = "game\\white_top_right.png";
-				break;
-			case "top":
-				grid[row][col].src = "game\\white_top.png";
-				break;
-			case "left":
-				grid[row][col].src = "game\\white_left.png";
-				break;
-			case "right":
-				grid[row][col].src = "game\\white_right.png";
-				break;
-			case "bottom":
-				grid[row][col].src = "game\\white_bottom.png";
-				break;
-			case "bottom_left":
-				grid[row][col].src = "game\\white_bottom_left.png";
-				break;
-			case "bottom_right":
-				grid[row][col].src = "game\\white_bottom_right.png";
-				break;
-			case "center":
-				grid[row][col].src = "game\\white.png";
-				break;
-		}	
+				case "top_left":
+					grid[row][col].src = "game\\white_top_left.png";
+					break;
+				case "top_right":
+					grid[row][col].src = "game\\white_top_right.png";
+					break;
+				case "top":
+					grid[row][col].src = "game\\white_top.png";
+					break;
+				case "left":
+					grid[row][col].src = "game\\white_left.png";
+					break;
+				case "right":
+					grid[row][col].src = "game\\white_right.png";
+					break;
+				case "bottom":
+					grid[row][col].src = "game\\white_bottom.png";
+					break;
+				case "bottom_left":
+					grid[row][col].src = "game\\white_bottom_left.png";
+					break;
+				case "bottom_right":
+					grid[row][col].src = "game\\white_bottom_right.png";
+					break;
+				case "center":
+					grid[row][col].src = "game\\white.png";
+					break;
+			}
+		}
+		return true;
+	} else {
+		return false;
 	}
 }
 
-// check if a person wins
+// check if a person wins. returns [win or not, win list(5 positions)]
+function checkWin(row, col) {
+	// check top left
+	if (grid[row][col].src.match(currentPlayer) && 
+	grid[row - 1][col - 1].src.match(currentPlayer) &&
+	grid[row - 2][col - 2].src.match(currentPlayer) && 
+	grid[row - 3][col - 3].src.match(currentPlayer) &&
+	grid[row - 4][col - 4].src.match(currentPlayer)) {
+		alert("yes");
+	}
+	
+	// check top
+	if (grid[row][col].src.match(currentPlayer) && 
+	grid[row - 1][col].src.match(currentPlayer) &&
+	grid[row - 2][col].src.match(currentPlayer) && 
+	grid[row - 3][col].src.match(currentPlayer) &&
+	grid[row - 4][col].src.match(currentPlayer)) {
+		alert("yes");
+	}
+	
+	// check top right
+	if (grid[row][col].src.match(currentPlayer) && 
+	grid[row - 1][col + 1].src.match(currentPlayer) &&
+	grid[row - 2][col + 2].src.match(currentPlayer) && 
+	grid[row - 3][col + 3].src.match(currentPlayer) &&
+	grid[row - 4][col + 4].src.match(currentPlayer)) {
+		alert("yes");
+	}
+	
+	// check left
+	if (grid[row][col].src.match(currentPlayer) && 
+	grid[row][col - 1].src.match(currentPlayer) &&
+	grid[row][col - 2].src.match(currentPlayer) && 
+	grid[row][col - 3].src.match(currentPlayer) &&
+	grid[row][col - 4].src.match(currentPlayer)) {
+		alert("yes");
+	}
+	
+	// check right
+	if (grid[row][col].src.match(currentPlayer) && 
+	grid[row][col + 1].src.match(currentPlayer) &&
+	grid[row][col + 2].src.match(currentPlayer) && 
+	grid[row][col + 3].src.match(currentPlayer) &&
+	grid[row][col + 4].src.match(currentPlayer)) {
+		alert("yes");
+	}
+	
+	// check bottom left
+	if (grid[row][col].src.match(currentPlayer) && 
+	grid[row + 1][col - 1].src.match(currentPlayer) &&
+	grid[row + 2][col - 2].src.match(currentPlayer) && 
+	grid[row + 3][col - 3].src.match(currentPlayer) &&
+	grid[row + 4][col - 4].src.match(currentPlayer)) {
+		alert("yes");
+	}
+	
+	// check bottom
+	if (grid[row][col].src.match(currentPlayer) && 
+	grid[row - 1][col].src.match(currentPlayer) &&
+	grid[row - 2][col].src.match(currentPlayer) && 
+	grid[row - 3][col].src.match(currentPlayer) &&
+	grid[row - 4][col].src.match(currentPlayer)) {
+		alert("yes");
+	}
+	
+	// check bottom right
+	if (grid[row][col].src.match(currentPlayer) && 
+	grid[row + 1][col + 1].src.match(currentPlayer) &&
+	grid[row + 2][col + 2].src.match(currentPlayer) && 
+	grid[row + 3][col + 3].src.match(currentPlayer) &&
+	grid[row + 4][col + 4].src.match(currentPlayer)) {
+		alert("yes");
+	}
+	
+	return [false];
+}
 
 function gridAction(elementId) {
 	var id = elementId;
 	var row_id = elementId[0];
 	var row = 0
 	var col = parseInt(elementId.slice(2, elementId.length));
+	var placed = false;
+	
 	switch(row_id) {
 		case "a":
 			row = 0;
@@ -171,36 +253,50 @@ function gridAction(elementId) {
 	// change texture
 	if (row == 0) {
 		if (col == 0) {
-			add("top_left");
+			placed = add("top_left", 0, 0);
 		}
 		else if (col == 14) {
-			add("top_right");
+			placed = add("top_right", 0, 14);
 		} else {
-			add("top");
+			placed = add("top", 0, col);
 		}
 	} else if (row == 14) {
 		if (col == 0) {
-			add("bottom_left");
+			placed = add("bottom_left", 14, 0);
 		}
 		else if (col == 14) {
-			add("bottom_right");
+			placed = add("bottom_right", 14, 14);
 		} else {
-			add("bottom");
+			placed = add("bottom", 14, col);
 		}
 	} else {
 		if (col == 0) {
-			add("left");
+			placed = add("left", row, 0);
 		} else if (col == 14) {
-			add("right");
+			placed = add("right", row, 14);
 		} else {
-			add("center");
+			placed = add("center", row, col);
 		}
 	}
 	
-	// switch player
-	if (currentPlayer == "black") {
-		currentPlayer = "white";
-	} else if (currentPlayer == "white") {
-		currentPlayer = "black";
+	// switch player or end game only if placement is successful
+	if (placed) {
+	
+		// check wins
+		if (checkWin(row, col)[0]) {
+			alert("win");
+		
+		} else {
+			if (currentPlayer == "black") {
+				currentPlayer = "white";
+				console.log("white player's turn");
+			} else if (currentPlayer == "white") {
+				currentPlayer = "black";
+				console.log("black player's turn");
+			}
+		}
+
+	} else {
+		console.log("invalid placement");
 	}
 }
